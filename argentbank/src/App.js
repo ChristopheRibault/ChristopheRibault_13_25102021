c4f901eecb70662eea30c0aee616c2f14d71a8d5
ChristopheRibault <c.ribault.dev@gmail.com>
2021-10-23 16:10:07 +0200

    feat: homepage layout

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,9 +1,13 @@
-import { Header } from './layout'
+import { Header, HeroContent, Features } from './layout'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <HeroContent />
+        <Features />
+      </main>
     </div>
   );
 }
--- argentbank/src/layout/index.js
+++ argentbank/src/layout/index.js
@@ -1,5 +1,9 @@
 import Header from '../layout/header';
+import HeroContent from './hero';
+import Features from './features';
 
 export {
   Header,
+  HeroContent,
+  Features,
 };
