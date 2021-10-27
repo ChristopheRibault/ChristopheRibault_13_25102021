0b28bc21a81c1402885d679e03fe5425f3894bfc
ChristopheRibault <c.ribault.dev@gmail.com>
2021-10-27 13:38:35 +0200

    fix: replace class attribute with className

--- argentbank/src/layout/header.jsx
+++ argentbank/src/layout/header.jsx
@@ -4,18 +4,18 @@ import logo from '../assets/img/argentBankLogo.png';
 const Header = function (props) {
   return (
     <header>
-      <nav class="main-nav">
-        <a class="main-nav-logo" href="./index.html">
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
           <img
-            class="main-nav-logo-image"
+            className="main-nav-logo-image"
             src={logo}
             alt="Argent Bank Logo"
           />
-          <h1 class="sr-only">Argent Bank</h1>
+          <h1 className="sr-only">Argent Bank</h1>
         </a>
         <div>
-          <Link class="main-nav-item" to="./login">
-            <i class="fa fa-user-circle"></i>{' '}
+          <Link className="main-nav-item" to="./login">
+            <i className="fa fa-user-circle"></i>{' '}
             Sign In
           </Link>
         </div>
