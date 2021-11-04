d61aaf91375d9c1fa03ce26f8cee8e152a633cf1
ChristopheRibault <c.ribault.dev@gmail.com>
2021-11-04 16:36:56 +0100

    feat: edit user info

--- argentbank/src/pages/userPage.jsx
+++ argentbank/src/pages/userPage.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from 'react';
 import store from '../utils/store';
 import * as usersActions from '../features/users';
-import { useFetch } from '../utils/hooks';
+import { useFetch, useForm } from '../utils/hooks';
+import fetcher from '../utils/axios';
 import { Account } from '../components';
 
 function UserPage() {
 
+  const [editionMode, setEditionMode] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
   const { isLoading, data, error } = useFetch({
     verb: 'post',
     url: '/user/profile',
+  });
+  
+  useEffect(() => {
+    setUserInfo(data)
+  }, [data]);
+
+  const { values, handleChange, handleSubmit } = useForm(async (values) => {
+    setEditionMode(false);
+
+    try {
+      const headers = {
+        'authorization': `Bearer ${localStorage.getItem('token')}`
+      };
+      fetcher.put('/user/profile', values, { headers });
+      setUserInfo(values);
+
+    } catch (error) {
+      console.log(error);
+    }
+
   });
 
   if (isLoading) {
@@ -25,10 +50,27 @@ function UserPage() {
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>Welcome back<br />{`${data.firstName} ${data.lastName}`}</h1>
-        <button className="edit-button">Edit Name</button>
+        <h1>Welcome back<br />{`${userInfo.firstName} ${userInfo.lastName}`}</h1>
+        {!editionMode &&
+          <button onClick={() => setEditionMode(true)} className="edit-button">Edit Name</button>
+        }
+        {editionMode && 
+          <form onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+              <label htmlFor="firstName">Firstname</label>
+              <input type="text" id="firstName" name="firstName" value={values.firstName || ''} onChange={handleChange} />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="lastName">Lastname</label>
+              <input type="text" id="lastName" name="lastName" value={values.lastName || ''} onChange={handleChange} />
+            </div>
+            <button type='submit' className="edit-button">Valider</button>
+          </form>
+        }
       </div>
+
       <h2 className="sr-only">Accounts</h2>
+      {/* Placeholder to be replaced by data fetched from server*/}
       <Account 
         title='Argent Bank Checking (x8349)'
         description='Available Balance'
